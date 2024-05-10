aef59453ec1586886201da441767d74485635a4c
Jar1c <mosjaric@gmail.com>
2024-05-10 12:24:20 +0800

    1

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-// Regular function for window scroll event
-window.onscroll = function() {
+
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -17,13 +15,13 @@ window.onscroll = function() {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+
             })
         }
     })
 }
-
-// Regular function for menu icon click event
-menuIcon.onclick = function() {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
